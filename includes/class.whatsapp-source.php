bccfbc7bbc5171ed080eabf65dcb6c0cc0a66f7c
valeriosza <valeriosza@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-22 01:55:33 +0000

    Update version 1.1
    git-svn-id: https://plugins.svn.wordpress.org/whatsapp-jetpack-button/tags/1.1@1250798 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/class.whatsapp-source.php
@@ -0,0 +1,84 @@
+<?php
+
+if( !function_exists('add_action') ) {
+	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+	exit;
+}
+class jetwhats_Share_WhatsApp extends Sharing_Source {
+	var $shortname = 'whatsapp';
+
+	function __construct( $id, array $settings ) {
+		parent::__construct( $id, $settings );
+
+		if( 'official' == $this->button_style )
+			$this->smart = true;
+		else
+			$this->smart = false;
+	}
+
+	function get_name() {
+		return __( 'WhatsApp', 'jetpack-whatsapp' );
+	}
+
+	function has_custom_button_style() {
+		return $this->smart;
+	}
+
+	private function guess_locale_from_lang( $lang ) {
+		if( strpos( $lang, 'ja' ) === 0 )
+			return 'ja';
+
+		if( strpos( $lang, 'zh' ) === 0 )
+			return 'zh-hant';
+
+		return 'en';
+	}
+
+	function get_display( $post ) {
+		include_once jetwhats__PLUGIN_DIR . 'includes/class.mobile.php';
+		$locale = $this->guess_locale_from_lang( get_locale() );
+		if ( wp_is_mobile()) {
+			//if ($iOS || $Android ) {
+				if( $this->smart )
+					return sprintf(
+						'<div class="whatsapp_button"><a href="whatsapp://send?text=%s: %s - %s%3Futm_source=jetpack-sharing%26utm_medium=whatsapp%26utm_campaign=mobile" class="share-whatsapp %s" title="%s"></a></div>',
+						__('Look this','jetpack-whatsapp'),
+						rawurlencode( $this->get_share_title( $post->ID ) ),
+						rawurlencode( $this->get_share_url( $post->ID ) ),
+						esc_attr( $locale ),
+						esc_attr__( 'WhatsApp it!', 'jetpack-whatsapp' )
+					);
+				else
+					return $this->get_link( get_permalink( $post->ID ), _x( 'WhatsApp', 'share to', 'jetpack-whatsapp' ), __( 'Click to share on WhatsApp', 'jetpack-whatsapp' ), 'share=whatsapp' );
+			//}
+		}
+	}
+
+	function display_header() {
+	}
+
+	function display_footer() {
+		$this->js_dialog( $this->shortname );
+	}
+
+	function process_request( $post, array $post_data ) {
+
+		$url = add_query_arg( array(
+		    'utm_source' => 'jetpack-sharing',
+		    'utm_medium' => 'whatsapp',
+		    'utm_campaign' => 'mobile'
+		), $this->get_share_url( $post->ID ) );
+
+		$params = array(
+		    'text' => __( 'Look this', 'jetpack-whatsapp' ) . ': ' . $this->get_share_title( $post->ID ).' - '.$url,
+		);
+
+		$whatsapp_url = 'whatsapp://send?' . http_build_query( $params );
+		// Record stats
+		parent::process_request( $post, $post_data );
+
+		// Redirect to WhatsApp
+		wp_redirect( $whatsapp_url );
+		die();
+	}
+}
